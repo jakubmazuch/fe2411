ff820a38ac34ec5563b582bfeffab8fca8fa6f69
Jakub Mazuch <jakub.mazuch@gmail.com>
2025-03-11 20:19:45 +0100

    11.3.2025, 20:19

---
+++ cast_2__js/lekce_7c/hlasovani.js
@@ -0,0 +1,21 @@
+let hlasySlon = 0;
+let hlasyTygr = 0;
+let hlasyZirafa = 0;
+
+const obrSlon = document.getElementById('obrSlon');
+const obrTygr = document.getElementById('obrTygr');
+const obrZirafa = document.getElementById('obrZirafa');
+
+const textSlon = document.getElementById('hlasySlon');
+const textTygr = document.getElementById('hlasyTygr');
+const textZirafa = document.getElementById('hlasyZirafa');
+
+obrSlon.addEventListener('click', function() {
+
+});
+obrTygr.addEventListener('click', function() {
+
+});
+obrZirafa.addEventListener('click', function() {
+
+});
