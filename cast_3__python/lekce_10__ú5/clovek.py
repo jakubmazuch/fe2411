3510dbec083228e879375c457446b67fcb72b8c1
Jakub Mazuch <jakub.mazuch@gmail.com>
2025-06-17 20:37:26 +0200

    17.6.2025, 20:38

--- cast_3__python/lekce_10__ú5/clovek.py
+++ cast_3__python/lekce_10__ú5/clovek.py
@@ -3,10 +3,15 @@ class Clovek:
         self.jmeno = jmeno
         self.prijmeni = prijmeni
         self.vek = vek
-        self.kamos = None
+        self.kamosi = [] # seznam kámošů
+    
+    def pridej_kamose(self, novy_kamos):
+        if novy_kamos not in self.kamosi:
+            self.kamosi.append(novy_kamos)
     
     def predstav_se(self):
-        if self.kamos:
-            print(f"Čau! Já jsem {self.jmeno} {self.prijmeni}, je mi {self.vek} let a můj kámoš je {self.kamos.jmeno} {self.kamos.prijmeni}.")
+        if self.kamosi:
+            seznam = ", ".join([f"{k.jmeno} {k.prijmeni}" for k in self.kamosi])
+            print(f"Čau! Já jsem {self.jmeno} {self.prijmeni}, je mi {self.vek} let a mí kámoši jsou {seznam}.")
         else:
             print(f"Čau! Já jsem {self.jmeno} {self.prijmeni}, je mi {self.vek} let a nemám žádné kámoše.")
--- cast_3__python/lekce_10__ú5/main.py
+++ cast_3__python/lekce_10__ú5/main.py
@@ -4,11 +4,21 @@ from clovek import Clovek
 karel = Clovek("Karel", "Novák", 33)
 cyril = Clovek("Cyril", "Nový", 27)
 jana = Clovek("Jana", "Zelená", 22)
+eva = Clovek("Eva", "Kolínská", 36)
+kai = Clovek("Kai", "Janů", 78)
 
 # seznamka
-karel.kamos = cyril
-cyril.kamos = jana
+karel.pridej_kamose(cyril)
+karel.pridej_kamose(eva)
+karel.pridej_kamose(kai)
+jana.pridej_kamose(eva)
+eva.pridej_kamose(jana)
+kai.pridej_kamose(cyril)
+kai.pridej_kamose(eva)
 
 # představování
 karel.predstav_se()
 cyril.predstav_se()
+jana.predstav_se()
+eva.predstav_se()
+kai.predstav_se()
