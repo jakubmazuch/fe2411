5cace8761e3fe69af24007afedf6d828585eac95
Jakub Mazuch <jakub.mazuch@gmail.com>
2025-07-01 20:23:59 +0200

    1.7.2025, 20:23

--- cast_3_2__python_django/mujprojekt/ahoj_svete/views.py
+++ cast_3_2__python_django/mujprojekt/ahoj_svete/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Ahoj světe!")
+    return render(request, 'ahoj_svete/index.html')
